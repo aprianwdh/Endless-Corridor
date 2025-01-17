ea43d01ad06408b4f77df3b1296d0b8abb72de83
aprianwdh <aprianwidhiwibowo@gmail.com>
2025-01-17 19:46:43 +0700

    17-1-25

--- Scripts/Core/trigger.gd
+++ Scripts/Core/trigger.gd
@@ -2,11 +2,15 @@ extends Area3D
 
 @export var animation : AnimationPlayer
 @export var anim_name : String
-
+@export var sound_trigger : AudioStreamPlayer
 
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
+		if sound_trigger != null:
+			sound_trigger.play()
+		else:
+			print("sound not found")
 		animation.play(anim_name)
 		await animation.animation_finished
 		queue_free()
