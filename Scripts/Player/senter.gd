0f5367a9e3d31eb338ff0a37cb9b6c2cd6dbaa38
aprianwdh <aprianwidhiwibowo@gmail.com>
2025-01-21 20:46:00 +0700

    .

--- Scripts/Player/senter.gd
+++ Scripts/Player/senter.gd
@@ -1,13 +1,23 @@
 extends SpotLight3D
 
+@onready var texture_battery = $"../../UI_Manager/battery/texture_battery"
+var battery_drain = 0.05
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print("flashligh visible "+str(visible))
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("senter") && GlobalScript.have_senter:
+	texture_battery.visible = visible
+	if Input.is_action_just_pressed("senter") && GlobalScript.have_senter && texture_battery.value != 0:
 		visible = !visible
 		$toggle.play()
+	if visible:
+		texture_battery.value -= battery_drain * delta
+	if texture_battery.value == 0 && visible == true:
+		visible = false
+		texture_battery.visible = false
+		
